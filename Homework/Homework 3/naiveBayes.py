23a050ed27ece5770faed113aea07848c2affd00
jacobalmon <jacobalmon03@gmail.com>
2024-10-03 09:02:00 -0700

    update hw3

--- Homework/Homework 3/naiveBayes.py
+++ Homework/Homework 3/naiveBayes.py
@@ -11,7 +11,7 @@ def split_data(file):
     # Spliting Data
     train_data = dataset[:20] # First 20 Entries.
     test_data = dataset[20:] # Last 10 Entries.
- 
+
     return train_data, test_data
 
 def prior_probability(train_data):
@@ -23,7 +23,7 @@ def prior_probability(train_data):
     for msg in train_data:
         if msg[0] == 'spam':
             count_spam += 1
-    
+
     # Calculates Probability of Spam.
     prob_spam = count_spam / sample_space
     # Take the Complement to Calculate Probability of Ham.
@@ -46,7 +46,7 @@ def probability_words(train_data):
             for word in words:
                 spam_word_count[word] += 1
                 total_spam_words += 1
-        
+
         elif label == 'ham': # Update words in dict if ham.
             for word in words:
                 ham_word_count[word] += 1
@@ -59,8 +59,8 @@ def cond_probability(sentence, pw_spam, pw_ham, pp_spam, pp_ham, num_words, tota
     words = sentence.split()
 
     # Temporary Probability for the Sentence.
-    p_spam = 1
-    p_ham = 1
+    p_spam = pp_spam
+    p_ham = pp_ham
 
     for word in words: # Calculating the Probability by Multiplying by Prior Probabibilties and Word Probabilities.
         p_spam *= (pw_spam.get(word, 0) + smoothing) / (total_spam_words + num_words)
@@ -88,20 +88,20 @@ def test_prediction(responses, data, num_sentence):
         # Update correctness if the data and the response matches.
         if responses[i] == data[i][0]:
             correct += 1
-    
+
     return correct / num_sentence
 
 if __name__ == "__main__":
     # Task 1.
-    train_data, test_data = split_data('SpamDetection.csv') 
+    train_data, test_data = split_data('SpamDetection.csv')
 
     # Task 2.
-    pp_spam, pp_ham = prior_probability(train_data) 
+    pp_spam, pp_ham = prior_probability(train_data)
     print(f'Prior Probability of Spam: {pp_spam}\n')
     print(f'Prior Probability of Ham: {pp_ham}\n')
 
     # Task 3.
-    pw_spam, pw_ham, total_spam_words, total_ham_words = probability_words(train_data) 
+    pw_spam, pw_ham, total_spam_words, total_ham_words = probability_words(train_data)
     num_words = find_num_words(train_data)
     for i in range(len(train_data)):
         pg_spam, pg_ham = cond_probability(train_data[i][1], pw_spam, pw_ham, pp_spam, pp_ham, num_words, total_spam_words, total_ham_words)
@@ -135,13 +135,11 @@ if __name__ == "__main__":
     num_sentence_test = len(test_data)
     responses_test = []
     for i in range(num_sentence_test):
-        pg_spam, pg_ham = cond_probability(test_data[i][1], pw_spam, pw_ham, pp_spam, pp_ham, num_words, total_spam_words, total_ham_words)
-        pgs_sentence = pp_spam * pg_spam
-        pgh_sentence = pp_ham * pg_ham
+        pgs_sentence, pgh_sentence = cond_probability(test_data[i][1], pw_spam, pw_ham, pp_spam, pp_ham, num_words, total_spam_words, total_ham_words)
 
         if pgs_sentence > pgh_sentence:
             classType = 'spam'
-            prob = pgs_sentence 
+            prob = pgs_sentence
         else:
             classType = 'ham'
             prob = pgh_sentence
@@ -151,7 +149,7 @@ if __name__ == "__main__":
         print(f'Posterior Probability of Sentence being Spam: {pgs_sentence}')
         print(f'Posterior Probability of Sentence being Ham: {pgh_sentence}')
         print(f'The Sentence is {classType}\n')
-        
+
     # Task 6.
     accuracy_train = test_prediction(responses_train, train_data, num_sentence_train)
     accuracy_test = test_prediction(responses_test, test_data, num_sentence_test)
